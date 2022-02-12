be735d06f16a446f1a9be13a25d11133fdab4e29
usmansheikh226 <m.usmansheikh226@gmail.com>
2022-02-12 10:18:45 +0500

    test message

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles do
-  resources :comments
+    resources :comments
+  end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your applica tion routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
